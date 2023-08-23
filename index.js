a1c884df4b3e9589a809a4604f3e976be2dc973e
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-08-23 14:02:36 +0600

    espresso coffe crud

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjxc9jf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffes');
+
+    app.get('/coffes', async (req, res) => {
+        const cursor = coffeeCollection.find();
+        const coffees = await cursor.toArray();
+        res.send(coffees);
+    });
+
+    app.post('/coffes',async(req, res) => {
+        const coffee = req.body;
+        const result = await coffeeCollection.insertOne(coffee);
+        res.send(result);
+    })
+
+    app.delete('/coffes/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Espresso Coffee is processing!');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
